13889e669641e9b866d44290d72b67b2f8e42b1f
Dharma Bonifacio <2123110279@untels.edu.pe>
2024-10-23 17:07:01 -0500

    Proyecto enviado de fragmentos

---
+++ AndroidStudioProjects/DAMSem09_Fragmentos_Virtual/app/src/main/java/com/example/damsem09_fragmentos_virtual/MainActivity.java
@@ -0,0 +1,93 @@
+package com.example.damsem09_fragmentos_virtual;
+
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.damsem09_fragmentos_virtual.databinding.ActivityMainBinding;
+
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    private AppBarConfiguration appBarConfiguration;
+    private ActivityMainBinding binding;
+    //Le ponemos true porque por defecto deberá cargar el nodo día
+    private boolean modoDía = true;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        setSupportActionBar(binding.toolbar);
+
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+
+        //acá creamos el botón para menú
+        binding.fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            //Este es el método que se ejecuta al hacer click en el toolbar
+            public void onClick(View view) {
+                //creamos  uanvariable
+                Fragment f= null;
+                if(modoDía){
+                    f = new SecondFragment();
+
+                }else{
+                    f= new FirstFragment();
+                }
+                modoDía = !modoDía;
+                getSupportFragmentManager().beginTransaction().replace(R.id.nav_host_fragment_content_main,f).commit();
+                /*Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAnchorView(R.id.fab)
+                        .setAction("Action", null).show();*/
+            }
+        });
+    }
+
+    //Opciones del menú
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+        return NavigationUI.navigateUp(navController, appBarConfiguration)
+                || super.onSupportNavigateUp();
+    }
+}
